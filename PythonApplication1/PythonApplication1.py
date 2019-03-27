2ca41bb6d67366bddbb1541e81320c33c3ee52ea
maruf amin <maruf10155@hotmail.com>
2019-03-26 20:34:43 -0400

    add board

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,13 +1,20 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
+height,width=600,600
+screen = pygame.display.set_mode((width, height))
 done = False
 clock = pygame.time.Clock()
 while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         done = True
-        screen.fill((255, 0, 0))
+        screen.fill((255, 255, 255))
+        for i in range(1,3):
+
+            pygame.draw.line(screen,(0,0,0),(i*width/3,0),(i*width/3,height))
+            pygame.draw.line(screen,(0,0,0),(0,i*height/3),(width,i*width/3))
+
+        
         pygame.display.flip()
         clock.tick(60)
 
